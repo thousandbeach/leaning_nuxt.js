7e1ae415f537353f4f583117d1582232c7d399ce
takabo <thousandbeach.td@gmail.com>
2019-01-17 00:45:12 +0900

    authorの子コンポーネントを作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,12 @@
     <div>jajaaj</div>
     <nuxt/>
     <div><h1>hahaha</h1></div>
+    <nuxt-child/>
   </div>
 </template>
 
 <style>
   body {
-    background-color: blue;
+    background-color: #7f828b;
   }
 </style>
---
+++ pages/author.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>author.vue</h1>
+    <nuxt-child/>
+  </div>
+</template>
